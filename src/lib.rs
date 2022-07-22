038565116d677463703b2928c074d521e476eb4f
Tristan Farmer <tristanfarmer@gmail.com>
2022-07-22 01:38:37 -0700

    refactor: Converted some logic to option ?

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+extern crate custom_error;
+use custom_error::custom_error;
 use conventional_commits_parser::parse_commit_msg;
-use git2::{Error, Repository, DescribeOptions, DescribeFormatOptions, ObjectType, Oid, Revwalk, Reference};
+use git2::{Repository, DescribeOptions, DescribeFormatOptions, ObjectType, Oid, Revwalk, Reference};
 use semver::{Version, Prerelease, BuildMetadata};
 
-#[derive(PartialEq, Eq)]
+custom_error! { pub Error
+    SemverError{source: semver::Error} = "Encountered an invalid version: {source}.",
+    GitError{source: git2::Error} = "Git Error: {source}"
+}
+
+#[derive(Clone, PartialEq, Eq)]
 enum VersionBump {
     MAJOR,
     MINOR,
@@ -68,31 +75,22 @@ pub fn run(repo_path: &str, is_release: bool) -> Result<String, Error> {
             version.build = BuildMetadata::EMPTY;
         }
     }
-
     Ok(version.to_string())
 }
 
 /// Checks if the provided Oid is a tagged revision in the Repository.
 /// Returns the name of the tag if found.
 fn get_revision_tag(repo: &Repository, oid: Oid) -> Option<String> {
-    if let Ok(mut tag_refs) = repo.references_glob("refs/tags/*") {
-        match tag_refs.find(does_reference_target_commit(oid)) {
-            Some(tag_item) => {
-                if let Ok(id) = tag_item {
-                    if let Some(t_name) = id.name() {
-                        return Some(t_name.to_owned());
-                    }
-                }
-            },
-            None => return None,
-        }
-    }
-    None
+    let mut tag_refs = repo.references_glob("refs/tags/*").ok()?;
+    let tag_item = tag_refs.find(does_reference_target_commit(oid))?;
+    let id = tag_item.ok()?;
+    let t_name = id.name()?;
+    return Some(t_name.to_owned());
 }
 
 /// Creates and returns a closure for determining if a Reference points to a given Oid/commit id.
-fn does_reference_target_commit(commit_id: Oid) -> impl FnMut(&Result<Reference, Error>) -> bool {
-    move |t_ref: &Result<Reference, Error>| {
+fn does_reference_target_commit(commit_id: Oid) -> impl FnMut(&Result<Reference, git2::Error>) -> bool {
+    move |t_ref: &Result<Reference, git2::Error>| {
         if let Ok(id) = t_ref {
             if let Some(coid) = id.peel_to_commit().ok() {
                 if coid.as_object().id() == commit_id {
@@ -114,20 +112,26 @@ fn derive_version_increase(repo: &Repository, mut refs: Revwalk) -> Result<Versi
         if let Some(_) = get_revision_tag(&repo, oid) {
             return Ok(VersionBumpDetails{bump_type, rev_count});
         }
-        let obj = repo.find_object(oid, Some(ObjectType::Commit))?;
-        if let Some(commit) = obj.as_commit() {
-            if let Some(commit_msg) = commit.message() {
-                if let Ok(parsed_commit) = parse_commit_msg(commit_msg) {
-                    if parsed_commit.is_breaking_change {
-                        bump_type = VersionBump::MAJOR;
-                    }
-                    if parsed_commit.ty == COMMIT_TYPE_FEAT && bump_type == VersionBump::PATCH {
-                        bump_type = VersionBump::MINOR;
-                    }
-                }
-            }
-        }
+        bump_type = match derive_version_from_commit(repo, oid, bump_type.clone()) {
+            Some(v) => v,
+            None => bump_type,
+        };
         rev_count += 1;
     }
     return Ok(VersionBumpDetails{bump_type, rev_count});
+}
+
+///
+fn derive_version_from_commit(repo: &Repository, oid: Oid, current_bump: VersionBump) -> Option<VersionBump> {
+    let obj = repo.find_object(oid, Some(ObjectType::Commit)).ok()?;
+    let commit = obj.as_commit()?;
+    let commit_msg = commit.message()?;
+    let parsed_commit = parse_commit_msg(commit_msg).ok()?;
+    if parsed_commit.is_breaking_change {
+        return Some(VersionBump::MAJOR);
+    }
+    if parsed_commit.ty == COMMIT_TYPE_FEAT && current_bump == VersionBump::PATCH {
+        return Some(VersionBump::MINOR);
+    }
+    Some(current_bump)
 }
